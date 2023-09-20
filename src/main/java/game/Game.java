1251b77637ced001a77572697f6760d0a9daed6b
Margarita Mingazova <zoomyzoomy@icloud.com>
2023-09-20 23:50:19 +0300

    Start use maps for faster searhing

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -3,52 +3,43 @@ package game;
 import player.Player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Game {
 
-    List<Player> players = new ArrayList<>();
+    HashMap<String,Integer> players = new HashMap<>();
     public void register(Player player){
-        players.add(player);
+        players.put(player.getName(),player.getStrength());
     }
 
-    public Player FindByName(String name){
-        for(Player player : players){
-            if (player.getName().equals(name)){
-                return player;
+    public Integer FindByName(String name){
+        for(String key : players.keySet()){
+            if (key.equals(name)){
+                Integer value = players.get(key);
+                return value;
             }
         }
         return null;
     }
 
     public int round(String playerName1, String playerName2){
-        Player player1 = FindByName(playerName1);
-        Player player2 = FindByName(playerName2);
+        Integer player1 = FindByName(playerName1);
+        Integer player2 = FindByName(playerName2);
 
-        for (Player player : players){
-            if (player.getName().equals(player1)){
-                player1 = player;
-            }
-            if (player.getName().equals(player2)){
-                player2 = player;
-            }
-        }
-
-        if (player1 == null){
+        if (player1 == null) {
             throw new NotRegisteredException(playerName1);
         }
-        if (player2 == null){
+        if (player2 == null) {
             throw new NotRegisteredException(playerName2);
         }
-
-        if (player1.getStrength()>player2.getStrength()){
+        if (player1 > player2) {
             return 1;
         }
-        if (player2.getStrength()>player1.getStrength()){
+        if (player1 < player2) {
             return 2;
         }
-
-       return 0;
+        return 0;
     }
 
 }
